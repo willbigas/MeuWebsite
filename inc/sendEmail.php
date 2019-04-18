2b99e2287d44ee2d45e0be2981326bcf267b0ad3
willbigas <williambmauro@hotmail.com>
2019-04-18 16:00:24 -0300

    updated project

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,7 +1,7 @@
 ﻿<?php
 
 // Replace this with your own email address
-$siteOwnersEmail = 'user@website.com';
+$siteOwnersEmail = 'williambmauro@hotmail.com';
 
 
 if($_POST) {
@@ -13,26 +13,26 @@ if($_POST) {
 
    // Check Name
 	if (strlen($name) < 2) {
-		$error['name'] = "Please enter your name.";
+		$error['name'] = "Por favor Digite seu nome.";
 	}
 	// Check Email
 	if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
-		$error['email'] = "Please enter a valid email address.";
+		$error['email'] = "Por favor digite um Endereco de E-mail valido.";
 	}
 	// Check Message
 	if (strlen($contact_message) < 15) {
-		$error['message'] = "Please enter your message. It should have at least 15 characters.";
+		$error['message'] = "Por favor digite sua mensagem. Ela deve conter no minimo 15 Caracteres.";
 	}
    // Subject
 	if ($subject == '') { $subject = "Contact Form Submission"; }
 
 
    // Set Message
-   $message .= "Email from: " . $name . "<br />";
-	$message .= "Email address: " . $email . "<br />";
-   $message .= "Message: <br />";
+   $message .= "Email De: " . $name . "<br />";
+	$message .= "Email Endereco: " . $email . "<br />";
+   $message .= "Mensagem: <br />";
    $message .= $contact_message;
-   $message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
+   $message .= "<br /> ----- <br /> Este Email foi recebido de seu Site Pessoal. <br />";
 
    // Set From: header
    $from =  $name . " <" . $email . ">";
@@ -50,7 +50,7 @@ if($_POST) {
       $mail = mail($siteOwnersEmail, $subject, $message, $headers);
 
 		if ($mail) { echo "OK"; }
-      else { echo "Something went wrong. Please try again."; }
+      else { echo "Algo deu errado. Por favor, tente novamente."; }
 		
 	} # end if - no validation error
 
